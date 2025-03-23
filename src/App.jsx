f0577754f22676b4432e6882c43427e4f1c56530
Leo Nickl <leo@leonickl.de>
2025-03-23 01:23:12 +0100

    format uuid field

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,9 @@ export default function App() {
                     className="bg-gray-800 min-h-10 w-max px-5 py-2 rounded-md border border-gray-400 focus:border-blue-700 focus:outline-blue-700"
                 />
 
-                <p>UUID: {uuid}</p>
+                <pre className="opacity-40 bg-gray-800 min-h-10 w-max px-5 py-2 rounded-md border border-gray-400 focus:border-blue-700 focus:outline-blue-700">
+                    {uuid}
+                </pre>
             </div>
 
             <div className="w-full flex flex-row gap-5 h-16">
