7e31e3e049948e507dd1cb8b8ceb1b58549f842b
Leo Nickl <leo@leonickl.de>
2025-03-24 14:43:29 +0100

    set cell data to 0 if created checkbox with empty content

--- src/SelectCellType.jsx
+++ src/SelectCellType.jsx
@@ -11,6 +11,12 @@ export default function SelectCellType() {
                 updateTable(cursor.y, cursor.x, {
                     type: e.target.value,
                 });
+
+                if (e.target.value === "checkbox" && !cell.data) {
+                    updateTable(cursor.y, cursor.x, {
+                        data: 0,
+                    });
+                }
             }}
             className="h-full bg-gray-800 w-40 px-5 py-2 rounded-md border border-gray-400 focus:border-blue-700 focus:outline-blue-700"
         >
