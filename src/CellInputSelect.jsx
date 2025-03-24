7e18a2f1cbb538a457a058914cab405b39ece27d
Leo Nickl <leo@leonickl.de>
2025-03-24 14:20:05 +0100

    handle foreign select items

--- src/CellInputSelect.jsx
+++ src/CellInputSelect.jsx
@@ -11,9 +11,32 @@ export default function CellInputSelect() {
         });
     }
 
+    const selectList = selectLists[cell.selectList];
+    const foreignItems = (cell.data ?? []).filter(
+        (item) => !selectList.includes(item)
+    );
+
     return (
         <div className="flex flex-row items-center justify-start gap-10 bg-gray-800 min-h-10 w-full px-5 py-2 rounded-md border border-gray-400 focus:border-blue-700 focus:outline-blue-700">
-            {selectLists[cell.selectList]?.map((item, index) => (
+            {selectList?.map((item, index) => (
+                <SelectItem
+                    key={item}
+                    item={item}
+                    color={colors[index % colors.length]}
+                    active={cell.data.includes(item)}
+                    toggleActive={() =>
+                        updateTable(cursor.y, cursor.x, {
+                            data: cell.data.includes(item)
+                                ? cell.data.filter((it) => it !== item)
+                                : [...cell.data, item],
+                        })
+                    }
+                />
+            ))}
+
+            {foreignItems.length > 0 && "|"}
+
+            {foreignItems.map((item, index) => (
                 <SelectItem
                     key={item}
                     item={item}
